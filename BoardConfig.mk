ec8e84464867f61c78dad876090fd720ecad67f0
CheckYourScreen <nimitmehta95@gmail.com>
2017-01-31 11:23:32 +0000

    onyx: enable qualcomm video enhancement flag
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,9 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := cm_onyx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
 
+# Add QC Video Enhancements flag
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
