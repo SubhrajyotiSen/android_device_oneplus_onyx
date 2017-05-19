d60856a9dc99b8fa802545587fbe72c3d0f1686e
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-05-19 23:36:58 +0530

    Switch to PN

--- aosp_onyx.mk
+++ aosp_onyx.mk
@@ -18,8 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/common.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
